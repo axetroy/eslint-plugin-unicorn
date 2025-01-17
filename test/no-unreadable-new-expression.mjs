baacb7d074e1f3cec08b6c970040b62b0707a652
Axetroy <axetroy.dev@gmail.com>
2025-01-17 17:01:57 +0800

    add more test

--- test/no-unreadable-new-expression.mjs
+++ test/no-unreadable-new-expression.mjs
@@ -6,8 +6,37 @@ const {test} = getTester(import.meta);
 test.snapshot({
 	valid: [
 		'const foo = new Foo();',
+		outdent`
+			const foo = new Foo();
+			const bar = foo.getBar();
+		`,
+		outdent`
+			const foo = new Foo()
+			const Bar = foo.Bar;
+		`,
+		outdent`
+			const {Bar} = foo;
+			const bar = new Bar();
+		`,
 	],
 	invalid: [
 		'const bar = new Foo().getBar();',
+		'const Bar = new Foo().Bar;',
+		'const Bar = new (Foo().Bar);',
+		'const bar = new foo.Bar();',
+		'const bar = new (foo.Bar)();',
+		'const bar = (new foo).Bar();',
+		'const bar = new foo().Bar();',
+		outdent`
+			function foo() {
+				return {
+					Bar: function () {}
+				}
+			}
+
+			const bar = new (foo().Bar)();
+		`,
+		`const bar = new ((0, Foo)).Bar();`,
+		`const bar = new ((() => Foo)()).Bar();`,
 	],
 });
